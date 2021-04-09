76afb61e6d1c0c059142970e90087744ae21d739
Tony Perri <tony.perri@progress.com>
2021-04-09 11:16:57 -0400

    Warning if results are paged

--- MOVEit.MIT/Private/Write-MITResponse.ps1
+++ MOVEit.MIT/Private/Write-MITResponse.ps1
@@ -12,19 +12,27 @@ function Write-MITResponse {
         [switch]$IncludePaging
     )
 
-    # Write out paging info to console if in verbose mode
-    if ($Response.paging) {
+    # Handle paging info in the response, if present
+    if ($Response.psobject.properties['paging']) {
+        # Write out paging info to console if in verbose mode
         Write-Verbose ("totalItems: {0} perPage: {1}  page: {2} totalPages: {3}" -f
                 $response.paging.totalItems, $response.paging.perPage, 
                 $response.paging.page, $response.paging.totalPages)
-    }
 
-    # Add type and write the paging info.  Want to send this down the pipeline first
-    # so the caller can check the first item for this information if they want
-    # to loop through multiple pages, etc.
-    if ($Response.paging -and $IncludePaging) {
-        $Response.paging.PSObject.TypeNames.Insert(0,'MITPaging')
-        $Response.paging 
+        if ($IncludePaging) {
+            # Add type and write the paging info.  Want to send this down the pipeline first
+            # so the caller can check the first item for this information if they want
+            # to loop through multiple pages, etc.
+            $Response.paging.PSObject.TypeNames.Insert(0,'MITPaging')
+            $Response.paging 
+        }
+        else {
+            # No paging info is being output, so write a warning if the total
+            # items exceeds the number displayed per page
+            if ($response.paging.totalItems -gt $response.paging.perPage) {
+                Write-Warning "Results are paged.  Use -IncludePaging, -Page and -PerPage parameters."
+            }
+        }
     }
 
     # Add type to the items for better display from .format.ps1xml file and write
