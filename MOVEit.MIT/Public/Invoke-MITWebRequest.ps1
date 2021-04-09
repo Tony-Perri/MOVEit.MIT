ea5685c0ebcb87a0a720d5208b9f92b60503f141
Tony Perri <tony.perri@progress.com>
2021-04-09 11:44:13 -0400

    Add Invoke-MITWebRequest

---
+++ MOVEit.MIT/Public/Invoke-MITWebRequest.ps1
@@ -0,0 +1,46 @@
+function Invoke-MITWebRequest {
+    <#
+    .SYNOPSIS
+        Cmdlet for invoking MOVEit Transfer REST methods that
+        there isn't a wrapper for yet.
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true,
+                    Position=1)]
+        [string]$Resource,
+
+        [Parameter(Mandatory=$false)]
+        [hashtable]$Query,
+
+        [Parameter(Mandatory=$false)]
+        [switch]$IncludePaging
+    )
+    
+    try {
+        Confirm-MITToken
+        # Confirm the token, refreshing if necessary
+
+        # Set the Uri for this request
+        $uri = "$script:BaseUri/$Resource"
+                
+        # Set the request headers
+        $headers = @{
+            Accept = "application/json"
+            Authorization = "Bearer $($script:Token.AccessToken)"
+        } 
+
+        # Additonal params that will be splatted
+        $irmParams = @{}
+        if ($Query) { $irmParams['Body'] = $Query}
+
+        # If this is PowerShell 7, lets add the -SkipHttpErrorCheck param
+        if ($PSVersionTable.PSVersion.Major -ge 7) { $irmParams['SkipHttpErrorCheck'] = $true}
+        
+        # Send the request and write out the response
+        Invoke-WebRequest -Uri $uri -Headers $headers @irmParams
+    }
+    catch {
+        $PSCmdlet.ThrowTerminatingError($PSItem)
+    }
+}
